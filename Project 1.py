9d3385e30eb4095febf32497075734729181760d
Lorenzoaustrin <lorenzoaustrin@gmail.com>
2024-05-18 19:04:45 +0530

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,11 @@
+# traffic light
+color=input('Enter the traffic light color')
+if(color=='green'):
+    print('u can go')
+elif(color=='red'):
+    print('u should stop')
+
+elif(color=='yellow'):
+    print('u should wait ')
+else:
+    print('there is not color in the traffic light')
